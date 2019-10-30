64074ad5b7d86d638e984933a8ae43d8d631199d
komaljituri <komaljituri88@gmail.com>
2019-10-30 17:28:21 +0530

    Updates

---
+++ precache-manifest.494f6ad4193748699e2718fee191566c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerApp/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "5f3582e6c710fa1939a7",
+    "url": "/burgerApp/static/js/runtime~main.3132dc1e.js"
+  },
+  {
+    "revision": "fc033949d386d7bf142d",
+    "url": "/burgerApp/static/js/main.96e083b2.chunk.js"
+  },
+  {
+    "revision": "c6bc87b1d3f9e4990a38",
+    "url": "/burgerApp/static/js/2.9732b096.chunk.js"
+  },
+  {
+    "revision": "fc033949d386d7bf142d",
+    "url": "/burgerApp/static/css/main.7a499e48.chunk.css"
+  },
+  {
+    "revision": "f646bb89dc6895944232ca4277372c2e",
+    "url": "/burgerApp/index.html"
+  }
+];
